16dca5910ff5880cbce48f89dd67a696074a5ac6
mmeo009 <shh020615@gmail.com>
2023-07-21 00:15:47 +0900

    DOTween

---
+++ Choroooo/Assets/02.Scripts/Framework/Core/Managers.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    static Managers s_instance;
+    static Managers Instance { get { Init(); return s_instance; } }
+
+    public static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if(go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
+
+    PoolManager _pool = new PoolManager();
+    ResourceManager _resource = new ResourceManager();
+    ObjectManager _object = new ObjectManager();
+    public static PoolManager pool { get { return Instance?._pool; } }
+    public static ResourceManager Resource { get { return Instance?._resource; } }
+    public static ObjectManager Object { get { return Instance?._object; } }
+}
+
